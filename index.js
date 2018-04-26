aa808e3fa72bf135af13ee78549762b22015ba18
astur <astur@yandex.ru>
2018-04-26 21:25:56 +0300

    code stub

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = () => {};
+module.exports = () => {
+    const store = {};
+    return {
+        collect: o => {
+            Object.keys(o).forEach(key => {
+                if(!store[key]) store[key] = [];
+                store[key].push(o[key]);
+            });
+        },
+        summary: () => store,
+    };
+};
