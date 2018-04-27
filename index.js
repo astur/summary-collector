5b8525c2176bf488c4a6192857e452c163b89d58
astur <astur@yandex.ru>
2018-04-27 20:27:37 +0300

    'store' option

--- index.js
+++ index.js
@@ -3,18 +3,15 @@ const mapObj = require('map-obj');
 const type = require('easytype');
 const arfy = require('arfy');
 
-module.exports = () => {
-    const store = {};
-    return {
-        collect: (o, ...args) => {
-            if(!type.isObject(o)) o = {[o]: arfy(...args)};
-            Object.keys(o).forEach(key => {
-                const vals = arfy(o[key]).filter(type.isNumber.finite);
-                if(!vals.length) return;
-                if(!store[key]) store[key] = [];
-                store[key] = [...store[key], ...vals];
-            });
-        },
-        summary: () => mapObj(store, (key, val) => [key, smry(val)]),
-    };
-};
+module.exports = ({store = {}} = {}) => ({
+    collect: (o, ...args) => {
+        if(!type.isObject(o)) o = {[o]: arfy(...args)};
+        Object.keys(o).forEach(key => {
+            const vals = arfy(o[key]).filter(type.isNumber.finite);
+            if(!vals.length) return;
+            if(!store[key]) store[key] = [];
+            store[key] = [...store[key], ...vals];
+        });
+    },
+    summary: () => mapObj(store, (key, val) => [key, smry(val)]),
+});
