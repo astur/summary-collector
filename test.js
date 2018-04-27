8218b5d14c83f3bdf7dae299d0fcdc6a845234dc
astur <astur@yandex.ru>
2018-04-27 14:45:02 +0300

    tests for collect params

--- test.js
+++ test.js
@@ -7,20 +7,46 @@ test('keys', t => {
     t.deepEqual(Object.keys(summary()), ['a', 'b', 'c', 'd']);
 });
 
-test('base', t => {
+test('numbers in object', t => {
     const {collect, summary} = require('.')();
     collect({a: 4});
     collect({a: 1});
     collect({a: 3});
     collect({a: 5});
     collect({a: 2});
-    t.deepEqual(summary(), {
-        a: {
-            min: 1,
-            max: 5,
-            sum: 15,
-            len: 5,
-            avg: 3,
-        },
-    });
+    t.deepEqual(summary().a, {min: 1, max: 5, sum: 15, len: 5, avg: 3});
+});
+
+test('array in object', t => {
+    const {collect, summary} = require('.')();
+    collect({a: [4, 1, 3, 5, 2]});
+    t.deepEqual(summary().a, {min: 1, max: 5, sum: 15, len: 5, avg: 3});
+});
+
+test('key, single number', t => {
+    const {collect, summary} = require('.')();
+    collect('a', 4);
+    collect('a', 1);
+    collect('a', 3);
+    collect('a', 5);
+    collect('a', 2);
+    t.deepEqual(summary().a, {min: 1, max: 5, sum: 15, len: 5, avg: 3});
+});
+
+test('key, array of numbers', t => {
+    const {collect, summary} = require('.')();
+    collect('a', [4, 1, 3, 5, 2]);
+    t.deepEqual(summary().a, {min: 1, max: 5, sum: 15, len: 5, avg: 3});
+});
+
+test('key, ...numbers', t => {
+    const {collect, summary} = require('.')();
+    collect('a', 4, 1, 3, 5, 2);
+    t.deepEqual(summary().a, {min: 1, max: 5, sum: 15, len: 5, avg: 3});
+});
+
+test('key, ...numbers&arrays', t => {
+    const {collect, summary} = require('.')();
+    collect('a', [4, 1], 3, [5], 2);
+    t.deepEqual(summary().a, {min: 1, max: 5, sum: 15, len: 5, avg: 3});
 });
