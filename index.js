700f50bc771e84ae50cde8ad70142923f16dd566
astur <astur@yandex.ru>
2018-04-27 15:18:07 +0300

    key+numbers params support

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const smry = require('smry');
 const mapObj = require('map-obj');
+const type = require('easytype');
+const arfy = require('arfy');
 
 module.exports = () => {
     const store = {};
     return {
-        collect: o => {
+        collect: (o, ...args) => {
+            if(!type.isObject(o)) o = {[o]: arfy(...args)};
             Object.keys(o).forEach(key => {
                 if(!store[key]) store[key] = [];
                 store[key] = [].concat(store[key], o[key]);
