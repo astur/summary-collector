d4598fcf6378516054c9d25f4e14329b04308eea
astur <astur@yandex.ru>
2018-05-08 05:47:06 +0300

    test for initial store check

--- test.js
+++ test.js
@@ -59,8 +59,8 @@ test('ignore bad values', t => {
 });
 
 test('initial store', t => {
-    const {summary} = require('.')({store: {a: [1, 2, 3, 4, 5]}});
-    t.deepEqual(summary().a, {min: 1, max: 5, sum: 15, len: 5, avg: 3});
+    const {summary} = require('.')({store: {a: [1, 2, 3, 4, 5, null], b: 'bad'}});
+    t.deepEqual(summary(), {a: {min: 1, max: 5, sum: 15, len: 5, avg: 3}});
 });
 
 test('quantile', t => {
