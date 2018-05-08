3d446474a6f8628ce7f6a4f60a5d9504867d3db8
astur <astur@yandex.ru>
2018-05-08 05:27:00 +0300

    refactoring mapObj

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const smry = require('smry');
-const mapObj = require('map-obj');
 const type = require('easytype');
 const arfy = require('arfy');
+const mapObj = require('lodash.mapvalues');
 
 const toFNA = v => arfy(v).filter(type.isNumber.finite);
 const incSmry = (o, a) => {
@@ -20,8 +20,8 @@ module.exports = ({
     quantile = [],
 } = {}) => {
     quantile = toFNA(quantile);
-    store = mapObj(store, (key, val) =>
-        [key, quantile.length ? val : smry(val)]);
+    store = mapObj(store, val =>
+        quantile.length ? val : smry(val));
     return {
         collect: (o, ...args) => {
             if(!type.isObject(o)) o = {[o]: arfy(...args)};
@@ -33,14 +33,8 @@ module.exports = ({
                     incSmry(store[key], vals);
             });
         },
-        summary: () => mapObj(store, (key, val) => [
-            key,
-            quantile.length ?
-                smry(val, {quantile}) :
-                mapObj(val, (key, val) => [
-                    key,
-                    +val.toFixed(10),
-                ]),
-        ]),
+        summary: () => mapObj(store, val => quantile.length ?
+            smry(val, {quantile}) :
+            mapObj(val, v => +v.toFixed(10))),
     };
 };
