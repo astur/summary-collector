8ed1da3412aa86111289b7b5a40a7f0cf83fc35e
astur <astur@yandex.ru>
2018-08-20 10:32:26 +0300

    test linted

--- test.js
+++ test.js
@@ -1,14 +1,15 @@
 const test = require('ava');
+const m = require('.');
 
 test('keys', t => {
-    const {collect, summary} = require('.')();
+    const {collect, summary} = m();
     collect({a: 1, b: 2});
     collect({c: 1, d: 2});
     t.deepEqual(Object.keys(summary()), ['a', 'b', 'c', 'd']);
 });
 
 test('numbers in object', t => {
-    const {collect, summary} = require('.')();
+    const {collect, summary} = m();
     collect({a: 4});
     collect({a: 1});
     collect({a: 3});
@@ -18,13 +19,13 @@ test('numbers in object', t => {
 });
 
 test('array in object', t => {
-    const {collect, summary} = require('.')();
+    const {collect, summary} = m();
     collect({a: [4, 1, 3, 5, 2]});
     t.deepEqual(summary().a, {min: 1, max: 5, sum: 15, len: 5, avg: 3});
 });
 
 test('key, single number', t => {
-    const {collect, summary} = require('.')();
+    const {collect, summary} = m();
     collect('a', 4);
     collect('a', 1);
     collect('a', 3);
@@ -34,50 +35,50 @@ test('key, single number', t => {
 });
 
 test('key, array of numbers', t => {
-    const {collect, summary} = require('.')();
+    const {collect, summary} = m();
     collect('a', [4, 1, 3, 5, 2]);
     t.deepEqual(summary().a, {min: 1, max: 5, sum: 15, len: 5, avg: 3});
 });
 
 test('key, ...numbers', t => {
-    const {collect, summary} = require('.')();
+    const {collect, summary} = m();
     collect('a', 4, 1, 3, 5, 2);
     t.deepEqual(summary().a, {min: 1, max: 5, sum: 15, len: 5, avg: 3});
 });
 
 test('key, ...numbers&arrays', t => {
-    const {collect, summary} = require('.')();
+    const {collect, summary} = m();
     collect('a', [4, 1], 3, [5], 2);
     t.deepEqual(summary().a, {min: 1, max: 5, sum: 15, len: 5, avg: 3});
 });
 
 test('ignore bad values', t => {
-    const {collect, summary} = require('.')();
+    const {collect, summary} = m();
     collect('a', 1, false, NaN, /^$/, {});
     collect('b', null, [true, undefined], '1');
     t.deepEqual(summary(), {a: {min: 1, max: 1, sum: 1, len: 1, avg: 1}});
 });
 
 test('initial store', t => {
-    const {summary} = require('.')({store: {a: [1, 2, 3, 4, 5, null], b: 'bad'}});
+    const {summary} = m({store: {a: [1, 2, 3, 4, 5, null], b: 'bad'}});
     t.deepEqual(summary(), {a: {min: 1, max: 5, sum: 15, len: 5, avg: 3}});
-    t.deepEqual(require('.')({store: 'bad'}).summary(), {});
+    t.deepEqual(m({store: 'bad'}).summary(), {});
 });
 
 test('quantile', t => {
-    const {collect, summary} = require('.')({quantile: [0.25, 0.5, 0.75, 'bad']});
+    const {collect, summary} = m({quantile: [0.25, 0.5, 0.75, 'bad']});
     collect('a', 1, 2, 3, 4);
     t.deepEqual(summary().a.quantile, {0.25: 1.25, 0.5: 2.5, 0.75: 3.75});
 });
 
 test('initial store with quantile', t => {
-    const {collect, summary} = require('.')({quantile: 0.5, store: {a: [1, 2]}});
+    const {collect, summary} = m({quantile: 0.5, store: {a: [1, 2]}});
     collect('a', 3, 4, 5);
     t.deepEqual(summary(), {a: {min: 1, max: 5, sum: 15, len: 5, avg: 3, quantile: {0.5: 3}}});
 });
 
 test('counters', t => {
-    const {collect, summary} = require('.')({counters: ['a', 'c'], store: {a: [1, 2]}});
+    const {collect, summary} = m({counters: ['a', 'c'], store: {a: [1, 2]}});
     collect('a', 3, 4, 5);
     collect('b', 1, 2, 3, 4, 5);
     t.is(summary().a, 15);
